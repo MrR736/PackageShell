fb3de978ef10b80262d8cbfa56c3b28c49775ebf
MrR736 <mrr6451@outlook.com>
2024-10-25 13:24:00 -0400

    Add files via upload

--- usr/scripts/PackageName/2.sh
+++ usr/scripts/PackageName/2.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-Scripts_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-if [ "$(whoami)" != "root" ]
-then
-  echo "You have to run as Superuser!"
-  exit 1
-fi
-
-# Check for required binaries
-for cmd in echo; do
-  if ! command -v "$cmd" &> /dev/null; then
-    echo "$cmd is missing!"
-    exit 1
-  fi
-done
-
 echo "2"
