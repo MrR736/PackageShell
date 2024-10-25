4f1bdaa337b9804bb37120a80298c175aab92ad9
MrR736 <mrr6451@outlook.com>
2024-10-25 13:14:34 -0400

    Update Remove.sh

--- Remove.sh
+++ Remove.sh
@@ -4,16 +4,20 @@ COMMAND_NAME="Package Name"
 SCRIPTS_BIN="/usr/bin/pn"
 SCRIPTS_FOLDER="/usr/scripts/PackageName"
 
-if [ "$(whoami)" != "root" ]
-then
-  echo "You Have To Run As Superuser!"
-  exit 1
+if [ "$(whoami)" != "root" ]; then
+    echo -e "\e[31mE: You have to run as Superuser\e[0m"
+    exit 1
 fi
 
 if [[ ! -e "$SCRIPTS_BIN" ]]; then
     echo "$COMMAND_NAME is Not Install, so Cancelling Removed."
     exit 1
 else
-    echo "Remove $COMMAND_NAME"
-    rm -rf "$SCRIPTS_BIN" "$SCRIPTS_FOLDER"
+    if ! command -v rm &> /dev/null; then
+        echo -e "\e[31mE: rm Is Not Installed.\e[0m"
+        exit 1
+    else
+        echo "Remove $COMMAND_NAME..."
+        rm -rf "$SCRIPTS_BIN" "$SCRIPTS_FOLDER"
+    fi
 fi
