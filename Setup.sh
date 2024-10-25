e213b082a4d61924389ecb19b7bd2504f9742129
MrR736 <mrr6451@outlook.com>
2024-10-25 13:13:18 -0400

    Add files via upload

--- Setup.sh
+++ Setup.sh
@@ -2,29 +2,35 @@
 
 SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ZZ_BIN="$SCRIPTS_DIR/7zz"
+Zips_DIR=$SCRIPTS_DIR/*.7z
 
-if [ "$(whoami)" != "root" ]; then
-    echo "You have to run as Superuser!"
+if [[ ! -e "$SCRIPTS_DIR/Install.list" ]]; then
+    echo -e "\e[31mE: Not Found Install.list\e[0m"
     exit 1
 fi
 
-if [[ ! -e "$ZZ_BIN" ]]; then
-    echo "7zz is missing from $SCRIPTS_DIR."
+if ! command -v $ZZ_BIN &> /dev/null; then
+    echo -e "\e[31mE: 7zz Is Not Installed.\e[0m"
+    exit 1
+fi
+
+if [[ ${#Zips_DIR[@]} -eq 0 ]]; then
+    echo -e "\e[31mE: Not Found .7z Files\e[0m"
+    exit 1
+fi
+
+if [ "$(whoami)" != "root" ]; then
+    echo -e "\e[31mE: You have to run as Superuser\e[0m"
     exit 1
 fi
 
 # Check for .list files
 shopt -s nullglob  # Avoid issues with empty glob results
-list_files=("$SCRIPTS_DIR"/Install.list)
-
-if [[ ${#list_files[@]} -eq 0 ]]; then
-    echo "E: No List Files found in $SCRIPTS_DIR."
-    exit 1
-fi
+list_files=($SCRIPTS_DIR/Install.list)
 
 for list_file in "${list_files[@]}"; do
     if [[ ! -s "$list_file" ]]; then
-        echo "E: List file $list_file is empty."
+        echo -e "\e[31mE: List File Install.list Is Empty.\e[0m"
         continue
     fi
 
@@ -54,10 +60,17 @@ for list_file in "${list_files[@]}"; do
         rm -rf "$INSTALL_COMMAND_FILE"
     fi
 
-    if $ZZ_BIN x "$SCRIPTS_DIR"/*.7z -O"/" -y > /dev/null; then
-        echo "Setup Package successful."
-    else
-        echo "E: Setup Package failed."
-        exit 1
-    fi
+    echo "Processe Package..."
+
+    for zip_file in $Zips_DIR; do
+        Install_Zips="${zip_file#$SCRIPTS_DIR/}"
+        if $ZZ_BIN x "$zip_file" -O"/" -y > /dev/null; then
+           echo "Extract $Install_Zips Successfully."
+        else
+           echo -e "\e[31mE: Extract $Install_Zips Is Failed."
+           exit 1
+        fi
+    done
+
+    echo "Setting up $INSTALL_NAME_FILE Is Successfully."
 done
